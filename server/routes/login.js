3a524246fc6736d6128f9dbaf889dddd49ab62fd
Ale <163041908+Alessanshita@users.noreply.github.com>
2024-04-19 10:04:51 -0300

    Update login.js

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,6 +1,7 @@
-import { Router } from 'express';
+/*import { Router } from 'express';
 import { LoginModel } from '../models/mysql/post.js';
 
 export const loginRouter = Router();
 
 loginRouter.post('/', LoginModel.getUser);
+*/
